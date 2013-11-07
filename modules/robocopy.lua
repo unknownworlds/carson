16a9355468ac705c650553aef0c9b00bb1c562cf
UWE-Brian <brianc@unknownworlds.com>
2013-11-07 11:22:56 -0800

    Fixed return code detection in robocopy module

--- modules/robocopy.lua
+++ modules/robocopy.lua
@@ -5,13 +5,13 @@ function robocopy.copy(src, dest, patterns)
     local result = 0
     for p = 1, #patterns do
 
-        local copyResult = os.execute("robocopy " .. src .. " " .. dest .. " " .. patterns[p] .. " /e")
-        if copyResult ~= 0 then
+        local _, _, copyResult = os.execute("robocopy " .. src .. " " .. dest .. " " .. patterns[p] .. " /e")
+        if copyResult > result then
             result = copyResult
         end
 
     end
 
-    return result == 0
+    return result <= 8
     
 end
