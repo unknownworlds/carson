dee004f6565cf708c82b5537d024fa51aebbf82c
UWE-Brian <brianc@unknownworlds.com>
2013-11-22 14:26:26 -0800

    Added mail.onerror() and status.getresults() functions

--- modules/mail.lua
+++ modules/mail.lua
@@ -1,5 +1,5 @@
 mail = { }
- 
+
 local mailexe      = os.getenv("MAIL_EXE")    or error("MAIL_EXE must be defined")
 local mailserver   = os.getenv("MAIL_SERVER") or error("MAIL_SERVER must be defined")
 local mailport     = os.getenv("MAIL_PORT")   or error("MAIL_PORT must be defined")
@@ -9,35 +9,49 @@ local mailpassword = os.getenv("MAIL_PASSWORD")
 -- Sends an e-mail to the specified recipient.
 function mail.send(to, subject, message)
 
-	local command = [["]] .. mailexe .. [["]]
-	
-	-- Create a file containing the message we want to send.
-	local messageFileName = os.tmpname()
-	local messageFile = io.open(messageFileName, "w")
-	messageFile:write(message)
-	messageFile:close()
-	
-	command = command .. [[ "]] .. messageFileName .. [["]]
-	
-	command = command .. [[ -to ]] .. to
-	command = command .. [[ -subject "]] .. subject .. [["]]
-	command = command .. [[ -server ]] .. mailserver
-	command = command .. [[ -port ]] .. mailport
-	
-	if mailuser then
-		command = command .. [[ -f ]] .. mailuser
-		command = command .. [[ -u ]] .. mailuser
-	end
-		
-	if mailpassword then
-		command = command .. [[ -pw ]] .. mailpassword
-	end
-	
-	command = command .. [[ -html ]]
+    local command = [["]] .. mailexe .. [["]]
+    
+    -- Create a file containing the message we want to send.
+    local messageFileName = os.tmpname()
+    local messageFile = io.open(messageFileName, "w")
+    messageFile:write(message)
+    messageFile:close()
+    
+    command = command .. [[ "]] .. messageFileName .. [["]]
+    
+    command = command .. [[ -to ]] .. to
+    command = command .. [[ -subject "]] .. subject .. [["]]
+    command = command .. [[ -server ]] .. mailserver
+    command = command .. [[ -port ]] .. mailport
+    
+    if mailuser then
+        command = command .. [[ -f ]] .. mailuser
+        command = command .. [[ -u ]] .. mailuser
+    end
+        
+    if mailpassword then
+        command = command .. [[ -pw ]] .. mailpassword
+    end
+    
+    command = command .. [[ -html ]]
 
-	os.capture([["]] .. command .. [["]])
-	os.remove(messageFileName)
-	
+    os.capture([["]] .. command .. [["]])
+    os.remove(messageFileName)
+    
+end
+
+local function mailerror(mailToAddress)
+
+    if success then
+        return
+    end
+
+    mail.send(mailToAddress, "Build Failure", _LOG)
+
+end
+
+function mail.onerror(mailToAddress)
+    os.atexit(function(success) mailerror(success, mailToAddress) end)
 end
 
 return mail
--- modules/status.lua
+++ modules/status.lua
@@ -9,10 +9,18 @@ local endStr = [[
     <div class="status">%s</div>
 </div>]]
 
+local results = { total = 0, failures = 0 }
+
 function status.runstep(step, work)
 
     print(string.format(beginStr, step))
     local result = work()
+    results.total = results.total + 1
+    results.failures = results.failures + ((result == true) and 0 or 1)
     print(string.format(endStr, (result == true) and "success" or "failed"))
 
+end
+
+function status.getresults()
+    return results
 end
